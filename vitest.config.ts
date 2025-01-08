facc9e556778c0b2718e553b467f7333cbb0dab6
rstainsby <rstainsbydevelopment@gmail.com>
2025-01-08 16:36:24 +1000

    tidy: remove unnecessary vitest configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,4 @@
 /// <reference types="vitest" />
 import { getViteConfig } from "astro/config";
-import icon from 'astro-icon';
 
-export default getViteConfig({}, {
-  integrations: [icon({
-    iconDir: 'src/assets/icons',
-  })],
-});
+export default getViteConfig({});
