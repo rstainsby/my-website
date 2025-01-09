57afde2ad90a7de5f72f064dd508efd714cc7298
rstainsby <rstainsbydevelopment@gmail.com>
2025-01-09 16:36:32 +1000

    devops: install and configure netlify for deployments

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import icon from 'astro-icon';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon({
     iconDir: 'src/assets/icons',
-  })]
+  })],
+
+  adapter: netlify({
+    imageCDN: false,
+  })
 });
